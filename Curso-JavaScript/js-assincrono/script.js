e6f9c94c544e5605355e7a1c0639f14d16b6a608
Jefferson Santos <dalvatechnologies@gmail.com>
2022-04-11 15:13:29 -0300

    dei uma atualizada

--- Curso-JavaScript/js-assincrono/script.js
+++ Curso-JavaScript/js-assincrono/script.js
@@ -2,7 +2,6 @@ function ajax_get(url, callback) {
   const xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      console.log("responseText:" + xmlhttp.responseText);
       try {
         var data = JSON.parse(xmlhttp.responseText);
       } catch (err) {
@@ -12,7 +11,6 @@ function ajax_get(url, callback) {
       callback(data);
     }
   };
-
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
 }
